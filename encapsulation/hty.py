a12f67ebd5f6eb890cf62afc265bd077c8190b5e
abhishrmpy <abhishrm9072@gmail.com>
2025-08-05 20:15:31 +0530

    magic method

--- encapsulation/hty.py
+++ encapsulation/hty.py
@@ -1,10 +1,10 @@
 class Item:
-    def __init__(self, name:str, price:float, quantity=0):
+    def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        assert quantity >=0, f"quantity {quantity} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
 
-        #Assign to self object
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -12,8 +12,8 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
-item1 = Item("laptop", 5, 500)
-item2 = Item("phone", 4, 400)
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+
 print(item1.calculate_total_price())
 print(item2.calculate_total_price())
-    
