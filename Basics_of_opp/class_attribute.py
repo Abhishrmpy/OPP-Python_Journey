b111a465b8b34fa1cfefb479a1d25c3dec50fc8d
abhishrmpy <abhishrm9072@gmail.com>
2025-08-06 23:29:53 +0530

    learn class attribute

--- Basics_of_opp/class_attribute.py
+++ Basics_of_opp/class_attribute.py
@@ -1,4 +1,6 @@
 class Item:
+    pay_rate = 0.8
+    all = [] # class attribute to store all instances of item
     def __init__(self, name:str, price:float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
@@ -9,17 +11,28 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        #Action to excute
+        Item.all.append(self)
+
     #This is the action we want to perform in our class
     def calculate_total_price(self):
         return self.price * self.quantity
     
-#This is how we create an instance of the Item class
-item1 = Item("laptop", 5, 500)
-item2 = Item("phone", 4, 400)
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+    def apply_dicount(self):
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price}',{self.quantity})"
 
 
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
+# for instance in Item.all:
+#     print(instance.name)
 
+print(Item.all)
     
