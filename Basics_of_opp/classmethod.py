daca6c265686202c7b24e4155e865140eb668801
Abhishek <abhishrm9072@gmail.com>
2025-08-16 00:54:06 +0530

    updated the csv file

--- Basics_of_opp/classmethod.py
+++ Basics_of_opp/classmethod.py
@@ -1,47 +1,39 @@
-
 import csv
-
 
 class Item:
     pay_rate = 0.8
-    all = [] # class attribute to store all instances of item
-    def __init__(self, name:str, price:float, quantity=0):
-        # Run validations to the received arguments
-        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        assert quantity >=0, f"quantity {quantity} is not greater than or equal to zero!"
+    all = []
 
-        #Assign to self object
+    def __init__(self, name: str, price: float, quantity=0):
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
         self.name = name
         self.price = price
         self.quantity = quantity
 
-        #Action to excute
         Item.all.append(self)
 
-    #This is the action we want to perform in our class
     def calculate_total_price(self):
         return self.price * self.quantity
-    
-    def apply_dicount(self):
+
+    def apply_discount(self):
         self.price = self.price * self.pay_rate
 
     @classmethod
     def instantiate_from_csv(cls):
         with open('C:/pythonopps/OPP-Python_Journey/Basics_of_opp/items.csv', 'r') as f:
             reader = csv.DictReader(f)
-            items = list(reader)
-
-        for item in items:
-          
-            Item(
-                name = item.get("name"),
-                price = item.get("price"),
-                quantity = item.get("quantity"),
+            for row in reader:
+                cls(
+                    name=row.get("name"),
+                    price=float(row.get("price", 0)),
+                    quantity=int(row.get("quantity", 0))
                 )
 
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
+
 Item.instantiate_from_csv()
 print(Item.all)
-    
